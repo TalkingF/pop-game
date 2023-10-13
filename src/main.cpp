d8cbe64418cb9eb2677ff02fd8bb74efd6b1499e
TalkingF <talkingfinger05@gmail.com>
2023-10-13 21:41:43 +1030

    feat: TPS based updating

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,20 @@ int main()
 {
 	InitWindow(800, 800, "raylib window");
 	SetTargetFPS(60);
-
+	float last_tick = 0.0;
+	int total_ticks = 0;
 	while (!WindowShouldClose()) {
-
 		BeginDrawing();
-		ClearBackground(BLACK);
-		DrawText("This is a test window :D", 300, 350, 20, RAYWHITE);
+		
+		last_tick += GetFrameTime();
+		
+		//20 TPS based updates
+		if (last_tick > 0.05) {
+			last_tick = 0;
+			total_ticks++;
+			ClearBackground(BLACK);
+			DrawText(TextFormat("%d", total_ticks), 300, 350, 20, RAYWHITE);
+		}
 		EndDrawing();
-
 	}
 }
